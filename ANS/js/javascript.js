102c647cb3616c81356f8e39f3782e33ec15c875
adi <akumatha@gmail.com>
2021-11-09 18:02:36 +0100

    first commit

---
+++ ANS/js/javascript.js
@@ -0,0 +1,24 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     window.addEventListener('scroll', function() {
+//         if (window.scrollY > 30) {
+//           document.getElementById('navbar_top').classList.add('fixed-top');
+//           // add padding top to show content behind navbar
+//           navbar_height = document.querySelector('.navbar').offsetHeight;
+//           document.body.style.paddingTop = navbar_height + 'px';
+//         } else {
+//           document.getElementById('navbar_top').classList.remove('fixed-top');
+//            // remove padding top from body
+//           document.body.style.paddingTop = '0';
+//         } 
+//     });
+//   }); 
+
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
